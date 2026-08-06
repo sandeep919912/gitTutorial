f09a819c3963954e5708535853ddb870541c9720
Sandeep pandit <sandeeppandit919912@gmail.com>
2026-08-06 22:21:07 +0530

    clenup file

--- FileHandlingAndBuffers-Streams/index.js
+++ FileHandlingAndBuffers-Streams/index.js
@@ -1,53 +1,8 @@
 const http = require('http');
-const fs = require('fs');
+const route = require('./routes');
 
-const server = http.createServer((req , res) => {
-    let url = req.url;
-    let method = req.method;
 
-    if(url === '/'){
-
-        fs.readFile("message.txt", "utf8", (err, value) => {
-
-        if (err) {
-            value = "No messages yet";
-        }
-
-        res.setHeader("Content-Type", "text/html");
-
-        res.end(`
-            <h1>${value}</h1>
-
-            <form action="/submit" method="POST">
-                <input type="text" name="message" placeholder="Enter your message" required>
-                <button>Send</button>
-            </form>
-        `);
-
-    });
-
-    } else if (req.url === "/submit" && req.method === "POST") {
-        let values = []
-
-        req.on('data' , (chunk) =>{
-            values.push(chunk);
-        })
-
-        req.on('end' , () => {
-            let parseBody = Buffer.concat(values).toString();
-            
-            fs.writeFile('message.txt' , parseBody.split('=')[1] , (err) => {
-                if(err){
-                    console.log(err);
-                    res.end('Error writing to file');
-                }
-                res.statusCode = 302;
-                res.setHeader('Location', '/'); 
-                res.end();
-            })
-        })
-    }
-})
+const server = http.createServer(route);
 
 server.listen(3000, () => {
     console.log("Server is running on port 3000");
---
+++ FileHandlingAndBuffers-Streams/routes.js
@@ -0,0 +1,59 @@
+const fs = require('fs');
+
+const requestHandler = (req, res) => {
+    let url = req.url;
+    let method = req.method;
+
+    if(url === '/'){
+
+        fs.readFile("message.txt", "utf8", (err, value) => {
+
+        if (err) {
+            value = "No messages yet";
+        }
+
+        res.setHeader("Content-Type", "text/html");
+
+        res.end(`
+            <h1>${value}</h1>
+
+            <form action="/submit" method="POST">
+                <input type="text" name="message" placeholder="Enter your message" required>
+                <button>Send</button>
+            </form>
+        `);
+
+    });
+
+    } else if (req.url === "/submit" && req.method === "POST") {
+        let values = []
+
+        req.on('data' , (chunk) =>{
+            values.push(chunk);
+        })
+
+        req.on('end' , () => {
+            let parseBody = Buffer.concat(values).toString();
+            
+            fs.writeFile('message.txt' , parseBody.split('=')[1] , (err) => {
+                if(err){
+                    console.log(err);
+                    res.end('Error writing to file');
+                }
+                res.statusCode = 302;
+                res.setHeader('Location', '/'); 
+                res.end();
+            })
+        })
+    }
+}
+
+const secondHandler = (req, res) => {
+    console.log("Second handler called")
+}
+
+module.exports = { requestHandler, secondHandler };
+
+// module.exports.handler=requestHandler;
+//module.exports.secondHandler=secondHandler;
+
