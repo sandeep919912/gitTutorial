3ba7e05e35232cde0abda6d7946bd5c96ea92aa2
Sandeep pandit <sandeeppandit919912@gmail.com>
2026-08-09 09:59:09 +0530

    routes

--- Express-series/Task1/app.js
+++ Express-series/Task1/app.js
@@ -7,8 +7,24 @@ app.use("/welcome", (req, res, next) => {
     next();
 });
 
-app.get('/welcome', (req, res) => {
-    res.send(`Welcome ${req.user}`);
+app.get('/orders', (req, res) => {
+    console.log("Here is the list of all orders");
+    res.send(`<h1>Here is the list of all orders</h1>`);
+});
+
+app.post('/orders', (req, res) => {
+    console.log("A new order has been created");
+    res.json({ message: "A new order has been created" });
+});
+
+app.get('/users', (req, res) => {
+    console.log("Here is the list of all users");
+    res.send(`<h1>Here is the list of all users</h1>`);
+});
+
+app.post('/users' , (req , res)=>{
+    console.log("A new user has been created");
+    res.json({ message: "A new user has been created" });
 });
 
 app.listen(3000, () => {
