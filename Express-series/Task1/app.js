d871e9461d6142131a66e37664511f0ed374e2fd
Sandeep pandit <sandeeppandit919912@gmail.com>
2026-08-09 10:15:15 +0530

    routes-2

--- Express-series/Task1/app.js
+++ Express-series/Task1/app.js
@@ -2,32 +2,33 @@ const express = require('express');
 
 const app = express();
 
-app.use("/welcome", (req, res, next) => {
-    req.user = "Guest"
+app.use((req,res,next)=>{
+    console.log("middleware checking....")
     next();
+})
+
+app.get("/products" , (req , res) =>{
+    res.send("<h1>Here is the list of all products</h1>")
+})
+
+app.post("/products" , (req , res) =>{
+    res.send("<h1>A new product has been added.</h1>")
 });
 
-app.get('/orders', (req, res) => {
-    console.log("Here is the list of all orders");
-    res.send(`<h1>Here is the list of all orders</h1>`);
+app.get("/categories" , (req , res) =>{
+    res.send("<h1>Here is the list of all categories</h1>")
+})
+
+app.post("/categories" , (req , res) =>{
+    res.send("<h1>A new category has been added.</h1>")
 });
 
-app.post('/orders', (req, res) => {
-    console.log("A new order has been created");
-    res.json({ message: "A new order has been created" });
+app.get("/*splat", (req, res) => {
+    res.status(404).send("<h1>404 - Page Not Found</h1>");
 });
 
-app.get('/users', (req, res) => {
-    console.log("Here is the list of all users");
-    res.send(`<h1>Here is the list of all users</h1>`);
-});
 
-app.post('/users' , (req , res)=>{
-    console.log("A new user has been created");
-    res.json({ message: "A new user has been created" });
-});
-
-app.listen(3000, () => {
-    console.log("Server is running on port 3000");
+app.listen(4000, () => {
+    console.log("Server is running on port 4000");
 });
 
