fbe617139e61f55224f8f168e774bb6314e1fc28
Sandeep pandit <sandeeppandit919912@gmail.com>
2026-08-09 11:07:44 +0530

    methods and url

--- Express-series/Task1/app.js
+++ Express-series/Task1/app.js
@@ -2,17 +2,31 @@ const express = require('express');
 
 const app = express();
 
-app.use((req,res,next)=>{
-    console.log("middleware checking....")
+app.use((req, res, next) => {
+    console.log(`${req.method} request made to ${req.url}`);
     next();
-})
+});
 
-app.get("/welcome/:username" , (req , res) => {
-    const name = req.params.username
-    const role = req.query.role
-    
-    res.send(`Welcome ${name}, your role is ${role}`)
-})
+app.get("/products", (req, res) => {
+    res.send("Here is the list of all products.");
+});
+
+
+app.post("/products", (req, res) => {
+    res.send("A new product has been added.");
+});
+
+
+app.get("/categories", (req, res) => {
+    res.send("Here is the list of all categories.");
+});
+
+
+app.post("/categories", (req, res) => {
+    res.send("A new category has been created.");
+});
+
+
 
 app.listen(4000, () => {
     console.log("Server is running on port 4000");
