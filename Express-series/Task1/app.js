ca483e6aba3a39695766e3bd778c3d41122abb3d
Sandeep pandit <sandeeppandit919912@gmail.com>
2026-08-09 11:02:56 +0530

    dynamic route

--- Express-series/Task1/app.js
+++ Express-series/Task1/app.js
@@ -7,26 +7,12 @@ app.use((req,res,next)=>{
     next();
 })
 
-app.get("/products" , (req , res) =>{
-    res.send("<h1>Here is the list of all products</h1>")
+app.get("/welcome/:username" , (req , res) => {
+    const name = req.params.username
+    const role = req.query.role
+    
+    res.send(`Welcome ${name}, your role is ${role}`)
 })
-
-app.post("/products" , (req , res) =>{
-    res.send("<h1>A new product has been added.</h1>")
-});
-
-app.get("/categories" , (req , res) =>{
-    res.send("<h1>Here is the list of all categories</h1>")
-})
-
-app.post("/categories" , (req , res) =>{
-    res.send("<h1>A new category has been added.</h1>")
-});
-
-app.get("/*splat", (req, res) => {
-    res.status(404).send("<h1>404 - Page Not Found</h1>");
-});
-
 
 app.listen(4000, () => {
     console.log("Server is running on port 4000");
