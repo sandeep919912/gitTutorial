617767e197852131bc4a414542e3067fd3f99f54
Sandeep pandit <sandeeppandit919912@gmail.com>
2026-08-31 23:40:12 +0530

    login frontend

---
+++ Expence-Tracker/Frontend/login.js
@@ -0,0 +1,27 @@
+const form = document.querySelector("form")
+const API = "http://localhost:3000/users/login"
+
+const email  = document.querySelector("#emailbox")
+const password  = document.querySelector("#passwordbox")
+
+
+form.addEventListener("submit" , async (e)=>{
+    e.preventDefault()
+
+    const formData = {
+        email:email.value,
+        password:password.value
+    }
+
+    console.log(formData)
+
+    // try {
+    //     const res = await axios.post(API , formData)
+    //     alert(res.data)
+    // } catch (error) {
+    //     alert(error.response.data.message)
+    // }
+
+    // email.value = ""
+    // password.value = ""
+})
