c45803198032427205717250c3a91bbe42eff5a7
Sandeep pandit <sandeeppandit919912@gmail.com>
2026-09-04 11:01:58 +0530

    optimisation - 1

--- Expence-Tracker/Backend/controllers/leaderboard.controller.js
+++ Expence-Tracker/Backend/controllers/leaderboard.controller.js
@@ -1,41 +1,50 @@
-const Expences = require("../models/expences.model")
-const Users = require("../models/user.model")
-
-const leaderBoard = async (req , res) => {
-    try {
-        const users = await Users.findAll({
-            include:{
-                model:Expences,
-                attributes:["productPrice"]
-            }
-        })
-
-        const leaderBoard = users.map((user) => {
-            const totalExpence = user.expences.reduce((total , expence) => {
-                return total + Number(expence.productPrice)
-            }, 0)
-
-            return {
-                id: user.id,
-                name:user.name,
-                totalExpence
-            }
-        })
+const { fn, col } = require("sequelize");
 
 
-        leaderBoard.sort((a, b) => b.totalSpent - a.totalSpent);
+const Expences = require("../models/expences.model");
+const Users = require("../models/user.model");
+
+const leaderBoard = async (req, res) => {
+  try {
+
+        const leaderBoard = await Users.findAll({
+            attributes: [
+                "id",
+                "name",
+                [
+                    fn("SUM", col("expences.productPrice")),
+                    "totalExpence"
+                ]
+            ],
+
+            include: [
+                {
+                    model: Expences,
+                    attributes: []
+                }
+            ],
+
+            group: ["Users.id", "Users.name"],
+
+            order: [
+                ["totalExpence", "DESC"]
+            ],
+
+            raw: true
+        });
 
         return res.status(200).json({
             leaderBoard
         });
 
-
     } catch (error) {
-          return res.status(500).json({
+
+        return res.status(500).json({
             message: "Something went wrong",
             error: error.message
         });
-    }
-}
 
-module.exports = {leaderBoard}
+    }
+};
+
+module.exports = { leaderBoard };
